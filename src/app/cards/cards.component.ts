367e27a54c7a03e8345032c58c46ebb71fc8b86a
yavseny <75095248+yavseny@users.noreply.github.com>
2022-07-03 17:48:15 -0400

    first commit

---
+++ src/app/cards/cards.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+export class CardsComponent implements OnInit {
+  @Input () title = '';
+  @Input () imageUrl =  '';
+  @Input () username = '';
+  @Input () content = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
